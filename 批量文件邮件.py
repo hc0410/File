825dce379374e2a051789517b476594628a5bf72
Jason Huang <huangchengaiq@qq.com>
2019-10-22 18:06:14 +0800

    Update 批量文件邮件.py

--- 批量文件邮件.py
+++ 批量文件邮件.py
@@ -13,18 +13,15 @@ from email import encoders
 sender_host = 'smtp.qq.com'  # 默认服务器地址及端口
 sender_user = '1206064486@qq.com'
 sender_pwd = 'ntidvudghbqgfjhg'
-sender_name = u'Jason'
+sender_name = u'Jason Huang'
 
-
-attach_path = r'D:\File\attach'   # 附件所在文件夹
-attach_type = ".xlsx"      # 附件后缀名，即类型
-addrBook = r'D:\File\receivers.csv'  # 邮件地址通讯录
-content_path = r"D:\File\content.txt"   # 邮箱正文内容.txt
-month = 201910 # 月份，每次发送前修改
-
+attach_path = r'D:\File\attach'  # 附件所在文件夹
+attach_type = ".xlsx"  # 附件后缀名，即类型
+addrBook = r'D:\File\测试邮箱组.csv'  # 邮件地址通讯录
+year = 2019  # 年份，每次发送前修改
+month = 10  # 月份，每次发送前修改
 
 # 根据输入的CSV文件，获取通讯录人名和相应的邮箱地址
-# addrs = {name : value}
 
 def getAddrBook(addrBook):
     with open(addrBook, 'r', encoding='gbk') as addrFile:
@@ -33,8 +30,28 @@ def getAddrBook(addrBook):
         value = []
         addrs = {}
         for row in reader:
-            name.append(row[0])
-            value.append(row[1])
+            if reader.line_num == 1:  # 跳过表头
+                continue;
+            else:
+                name.append(row[2])
+                value.append(row[4])
+    addrs = dict(zip(name, value))
+    return addrs
+
+# 根据输入的CSV文件，获取通讯录人名和相应的邮箱地址
+
+def getAddrcc(addrBook):
+    with open(addrBook, 'r', encoding='gbk') as addrFile:
+        reader = csv.reader(addrFile)
+        name = []
+        value = []
+        addrs = {}
+        for row in reader:
+            if reader.line_num == 1:  # 跳过表头
+                continue;
+            else:
+                name.append(row[2])
+                value.append(row[6])
     addrs = dict(zip(name, value))
     return addrs
 
@@ -53,7 +70,32 @@ def getRecvAddr(addrs, person_name):
 
 # 加载邮件内容
 
-mail_content = 'Test Test'
+mail_content = '''
+<p style="font-weight:bold">Dear：</p>
+<p>烦请核对附件2019年10月份此处为自变量，会随着月份的变化而修正月提成表。<br />
+如下请特别关注：<br />
+1、个人和团队进账数据<br />
+2、个人当月出单数，上月出单数，退款，计提点及提成<br />
+3、下放期内会员进账数据<br />
+4、团队当月0单人数<br />
+5、团标，团队提成是否发放<br />
+</p>
+<p>
+请您最晚于2019年10月30日此处为自变量，根据每月时间自定义修改下前回复，谢谢支持<br />
+</p>
+'''
+
+# 加载邮件内容
+footer = '''
+<p><br /></p>
+<p><img src="https://emailalina.oss-cn-beijing.aliyuncs.com/footer.jpg"></p>
+<p style="font-size:25px;font-weight:bold">高顿财税学院 | HR Dept</p>
+<p style="font-size:15px;font-weight:bold">Alina Zhang</p>
+<p>Tel：400-111-0518</p>
+<p>Mail：zhangling@goldenfinance.com.cn</p>
+<p>Web：www.goldenfinance.com.cn</p>
+<p>Add：上海市虹口区花园路171号A5高顿教育</p>
+'''
 
 
 # 添加附件
@@ -62,7 +104,8 @@ def addAttch(attach_file):
     att = MIMEBase('application', 'octet-stream')  # 这两个参数不知道啥意思，二进制流文件
     att.set_payload(open(attach_file, 'rb').read())
     # 此时的附件名称为****.xlsx，截取文件名
-    att.add_header('Content-Disposition', 'attachment', filename=(attach_file.split("\\")[-1].split(".")[0] + '-' + str(month) + '.xlsx'))
+    att.add_header('Content-Disposition', 'attachment',
+                   filename=(attach_file.split("\\")[-1].split(".")[0] + '-' + str(year) + str(month) + '.xlsx'))
     encoders.encode_base64(att)
     return att
 
@@ -76,19 +119,20 @@ def mailSend():
     for root, dirs, files in os.walk(attach_path):
         for attach_file in files:  # attach_file : ***_2_***.xlsx
             msg = MIMEMultipart('alternative')
-            att_name = attach_file.split(".")[0] 
-            subject = att_name + '-' + str(month)
+            att_name = attach_file.split(".")[0]
+            subject = att_name + '-' + str(year) + str(month)
             msg['Subject'] = subject  # 设置邮件主题
-            person_name = att_name # subject.split("_")[-1]
+            person_name = att_name  # subject.split("_")[-1]
 
             addrs = getAddrBook(addrBook)
             recv_addr = getRecvAddr(addrs, person_name)
-
+            cc_addr = getAddrcc(addrBook)
+            cc_recv_addr = getRecvAddr(cc_addr, person_name)
             msg['From'] = formataddr([sender_name, sender_user])  # 设置发件人名称
             # msg['To'] = person_name # 设置收件人名称
             msg['To'] = formataddr([person_name, recv_addr])  # 设置收件人名称
-            
-            msg.attach(MIMEText(mail_content,'plain','utf-8'))  # 正文  MIMEText(content,'plain','utf-8')
+            msg['cc'] = formataddr([person_name,cc_recv_addr]) # 设置抄送人
+            msg.attach(MIMEText(mail_content + footer, 'html', 'utf-8'))  # 正文
             attach_file = root + "\\" + attach_file
             att = addAttch(attach_file)
             msg.attach(att)  # 附件
@@ -96,8 +140,8 @@ def mailSend():
             print("已发送： " + person_name + " <" + recv_addr + ">")
         smtp.quit()
 
+
 if __name__ == '__main__':
-    print("Jason")
     mailSend()
     anykey = input("请按回车键（Enter）退出程序：")
     if anykey:
